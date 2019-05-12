52a0fc17de73edfdf1fb7229b7e1e2da4bae41cc
Casey Chance <caseyjchance@gmail.com>
2019-05-12 15:49:36 -0700

    Updates run with env

--- run.sh
+++ run.sh
@@ -1,12 +1,27 @@
 #!/bin/bash
 
+source .env
+
 docker build . -t uptonchance.com
-docker run \
-  -it \
-  --rm \
-  --name uptonchance.com \
-  -p 8000:8000 \
-  -v $(pwd)/src:/app/src \
-  -v $(pwd)/data:/app/data \
-  -v $(pwd)/gatsby-config.js:/app/gatsby-config.js \
-  uptonchance.com npm run develop
+if [[ $APP_ENV == "prod" ]]; then
+  docker run \
+    -it \
+    --rm \
+    --name uptonchance.com \
+    -p 8000:8000 \
+    -v $(pwd)/src:/app/src \
+    -v $(pwd)/data:/app/data \
+    -v $(pwd)/gatsby-config.js:/app/gatsby-config.js \
+    uptonchance.com
+else
+  docker run \
+    -it \
+    --rm \
+    --name uptonchance.com \
+    -p 8000:8000 \
+    -v $(pwd)/src:/app/src \
+    -v $(pwd)/data:/app/data \
+    -v $(pwd)/gatsby-config.js:/app/gatsby-config.js \
+    uptonchance.com \
+    npm run develop
+fi
